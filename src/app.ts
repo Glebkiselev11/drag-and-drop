6ff640136d68f503e08884abaf48b2074939fa07
glebkiselev <12bny23gleb@gmail.com>
2020-05-17 20:03:30 +0300

    type for project as class

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
+// Project Type
+enum ProjectStatus { Active, Finished }
+
+class Project {
+	constructor(
+		public id: string,
+		public title: string,
+		public description: string,
+		public people: number,
+		public status: ProjectStatus
+	) {}
+}
+
+
 // Project State Management
 class ProjectState {
 	private listeners: any[] = [];
-	private projects: any[] = [];
+	private projects: Project[] = [];
 	
 	// Singleton
 	private static instance: ProjectState;
@@ -23,12 +37,13 @@ class ProjectState {
 	}
 
 	public addProject(title: string, description: string, numOfPeople: number) {
-		const newProject = {
-			id: Math.random().toString(),
-			title: title,
-			description: description,
-			people: numOfPeople
-		};
+		const newProject = new Project(
+			Math.random().toString(),
+			title,
+			description,
+			numOfPeople,
+			ProjectStatus.Active
+		);
 
 		this.projects.push(newProject);
 		for (const listenerFn of this.listeners) {
@@ -92,7 +107,7 @@ class ProjectList {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLElement;
-	assignedProjects: any[];
+	assignedProjects: Project[];
 
 	constructor(private type: 'active' | 'finished') {
 		this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
